0df378b731efbcf92278a28d76291e53aa9c60c8
onionmccabbage <onionmccabbage@live.com>
2024-04-15 16:20:49 +0100

    using modules and import

--- using_import/my_code.py
+++ using_import/my_code.py
@@ -1,3 +1,5 @@
+import datetime
+
 # we can import from our own modules
 import my_util # this imports everything from the my_util module
 
@@ -8,3 +10,8 @@ import my_util # this imports everything from the my_util module
 if __name__ == '__main__':
     print(my_util.isOdd(7)) # True
     print(my_util.isOdd(72)) # False
+    print(f'Inside my_code: {__name__}') # __main__
+    # we can acess many built-in features
+    print(my_util.isOdd.__doc__)
+
+    # NB anything using __NNN__ is built in to Python (dunder)
--- using_import/my_util.py
+++ using_import/my_util.py
@@ -2,6 +2,9 @@
 
 # NB all functions are object in Python
 def isOdd(n): # this is how we define a function
+    '''we may write documentation
+    strings at the top 
+    of a code block'''
     answer = bool(n%2 != 0) # != means not equal
     return answer
 # remember - the code block ends when we stop indenting!
@@ -14,4 +17,4 @@ if __name__ == '__main__': # then we must be running this module directly
     print(isOdd(-99)) # True
     # When we run a module directly, python ALWAYS sets __name__ to __main__
     # When we run code via an import, python sets __name__ to the module name
-    print(__name__) # __main__
+    print(f'Inside my_util: {__name__}') # 
