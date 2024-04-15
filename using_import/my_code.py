1ec0e1b8d7fed2012fd77b7188cb3bb5e444950f
onionmccabbage <onionmccabbage@live.com>
2024-04-15 16:04:08 +0100

    why we use __name__ == __main__

--- using_import/my_code.py
+++ using_import/my_code.py
@@ -1,3 +1,10 @@
 # we can import from our own modules
 import my_util # this imports everything from the my_util module
 
+# now we can use our imported code
+# exactly as if it was written in THIS module
+
+# we should always use this check...
+if __name__ == '__main__':
+    print(my_util.isOdd(7)) # True
+    print(my_util.isOdd(72)) # False
--- using_import/my_util.py
+++ using_import/my_util.py
@@ -6,8 +6,12 @@ def isOdd(n): # this is how we define a function
     return answer
 # remember - the code block ends when we stop indenting!
 
-# we can exercise our code
-print(isOdd, type(isOdd))
-print(isOdd(3)) # True
-print(isOdd(2)) # False
-print(isOdd(-99)) # True
+if __name__ == '__main__': # then we must be running this module directly
+    # we can exercise our code
+    print(isOdd, type(isOdd))
+    print(isOdd(3)) # True
+    print(isOdd(2)) # False
+    print(isOdd(-99)) # True
+    # When we run a module directly, python ALWAYS sets __name__ to __main__
+    # When we run code via an import, python sets __name__ to the module name
+    print(__name__) # __main__
