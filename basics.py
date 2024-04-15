c3b15f5c56c11d8d5899696e7f9795e93568070d
onionmccabbage <onionmccabbage@live.com>
2024-04-15 10:21:15 +0100

    using strings

--- basics.py
+++ basics.py
@@ -18,4 +18,16 @@ h = b%a # show the remainder (and possibly a calculation error (epsilon) )
 print(g, type(g))
 print(h)
 
-
+# using strings (we can encode new line as \n and tab as \t)
+s = 'is it coffee\n time yet' # python can use single or double quotes
+s2 = '''we can use triple quotes (or triple double quotes) 
+which will preseve new lines                and other formating features'''
+# strings are an indexed collection of characters, accessible by slicing
+print(s2[0]) # we can access zero-based members of the collection
+print(s2[3:12]) # we can use slicing: [start:stop-before:step]
+j = s+s2
+k = s*4
+print(j, k)
+# more commonly we use join
+r = s.join('-')
+print(r)
