b6cb7ecef9ad6d98e104c3b67eff1664b58ff5e3
onionmccabbage <onionmccabbage@live.com>
2024-04-15 12:00:26 +0100

    using range

--- structure.py
+++ structure.py
@@ -3,6 +3,8 @@
 # we might loop until a valid entry is available
 while True: # loop endlessly - use with care!!!
     u = input('Please enter a value ') # this waits for a human to type something at the console
+    # validation
+    # conditional code
     if u == 'quit': # == compares values
         break # breaks out of the current loop
     elif u.isnumeric(): # check if the value contains ONLY numeric values
@@ -18,12 +20,17 @@ while True: # loop endlessly - use with care!!!
     else:
         print(u, type(u))
 
-# validation
-# conditional code
-
 
 # iterating over collections
+# in this case, we will generate a list containing all the square nubmers from 1 to 10
+sq_l = [] # an empty list (we might end up with duplicate values)
+# be careful - there is NEVER a predictable order for the members of a set (or of a dict)
+sq_set = {0} # a set containing one value (a set ensures only unique values)
+for i in range(10,-11, -1): # range will generate numbers from (start, stop-before, step)
+    sq_l.append(i*i) # square each integer
+    sq_set.add(i*i)
 
+print(sq_l, sq_set) # all the square numbers from 0 to 10
 
 # print formatting
 
