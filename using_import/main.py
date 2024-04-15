e284a0289e1675f5a45ac79cd4bcd8611410ae25
onionmccabbage <onionmccabbage@live.com>
2024-04-15 16:29:41 +0100

    end of day 1

---
+++ using_import/main.py
@@ -0,0 +1,14 @@
+# very often we have a main module
+# we may choose to import only PARTS Of another module
+from my_util import isOdd
+# this can also work for biult-in libraries
+from datetime import datetime
+
+def main(m): # by convention we call our main funtion 'main'
+    return isOdd(m)
+
+
+if __name__ == '__main__':
+    print(f'Today is {datetime.now()}')
+    r = main(3) # invoke the function called 'main'
+    print(r)
