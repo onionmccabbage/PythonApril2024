0648bfe681591e42eb592b5dd25db0ef679e3120
onionmccabbage <onionmccabbage@live.com>
2024-04-15 10:45:03 +0100

    other collections

--- basics.py
+++ basics.py
@@ -31,3 +31,35 @@ print(j, k)
 # more commonly we use join
 r = s.join('-')
 print(r)
+
+# there are several built in collections in Python
+# tuples, lists, sets and dictionaries
+# a tuple is an immutable indexed collection of any type
+t = (7 , 4, 9, True, s, 'this is clever') # () declare a tuple
+print(t[3]) # True
+# a list is an indexed collection of any type - can be mutated
+l = ['is', 'it', 'coffee', 'yet'] # [] declares a list (members may be of ANY types)
+l.append('?') # append as the last member
+l.pop() # remove the last member
+print(l[2])
+
+# we can use slicing in any indexed collection
+print(t[3:6])
+
+# we also have a 'set' - a collection of unique members of any type
+values = {3,4,4,5,8,2,3} # we can use {} to declare a set
+values.add(9)
+values.remove(4)
+print(values)
+
+# in conclusion: string and tuple are IMMUTABLE
+# list and set are MUTABLE
+
+# we aim to use tuples, since they use less resources (immutable)
+
+# a dict (dictionary) is a NON INDEXED mutable collection of any type
+# in Python the key may be of ANY data type, but we tend to use strings (even mixed key types)
+unusual = {1:'wow', True:'really?', 3.456:'pi'} # this is legal but not recommended
+# duplicate keys take the LAST-defined value
+d = {'fn':'Timnit', 'fn':'alternative', 'ln':'Gebru', 'age':43, 'other':l} # {} can also be used to declare a dict:a collection of name:value pairs
+print(d)
