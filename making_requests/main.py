0cd75dee4747c4b919c52faa8b2e4637774b8ae2
onionmccabbage <onionmccabbage@live.com>
2024-04-16 11:50:47 +0100

    make request with arguments (parameters)

--- making_requests/main.py
+++ making_requests/main.py
@@ -2,16 +2,23 @@ import requests # remember we may need to pip install requests
 # up until 10yrs ago, we used urllib.request
 
 
-def makeApiCall():
+def makeApiCall(whichId=False):
     '''make a request to a web API and deal with the returned JSON'''
-    url = 'https://jsonplaceholder.typicode.com/users'
+    if whichId:
+        url = f'https://jsonplaceholder.typicode.com/users/{whichId}'
+    else:
+        url = 'https://jsonplaceholder.typicode.com/users'
     # we make a request and grab any response
+    # NB respone.get, response.post, response.put etc.
     response = requests.get(url) # we now have a response object
+    # we might receive xml, text etc. from the API
+    # u = response.text() or u=response.html() etc.
     # populate a list from the JSON of the response
-    users_l = response.json() # this will convert the JSON part of the response into a list
-    return users_l
-
+    users = response.json() # this will convert the JSON part of the response into a list
+    return users
 
 if __name__ == '__main__':
-    users = makeApiCall()
+    # we may choose to pass an argument to our function
+    # users = makeApiCall() # with no argument, the default will be used
+    users = makeApiCall(6) # with an argument, the default is overriden
     print(users, type(users))
