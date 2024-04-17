d49c4e569d673ebe71dd37e2f1a5659112a08075
onionmccabbage <onionmccabbage@live.com>
2024-04-17 15:53:20 +0100

    using sys for platform-specific limits

---
+++ using_sys.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == '__main__':
+    print(sys.maxsize) # what is the largest object permitted in memory
+    print(sys.float_info) # the largest and smallest floating point values permitted
